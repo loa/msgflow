a6c8ea77abf67714b3b6150e1c2140bfefc1b85e
Carl Loa Odin <carlodin@gmail.com>
2010-12-13 23:08:37 +0100

    hd_head now gives time of latest post instead of time()

--- hd_head.php
+++ hd_head.php
@@ -4,17 +4,29 @@
 	FORUM_PAGE == 'viewforum'
 	FORUM_PAGE == 'viewtopic'
 */
-        require_once($ext_info['path'].'/../mobileswitch/mobile_device_detect.php');
-        $mob = mobile_device_detect(true,false,true);
+        $query = array(
+                'SELECT' => 'posted',
+                'FROM'		=> 'posts',
+                'ORDER BY'	=> 'posted DESC',
+                'LIMIT'         => '1'
+        );
+
+	$result = $forum_db->query_build($query) or error(__FILE__, __LINE__);
+
+        if($forum_db->num_rows($result) == 1) {
+	    $res = $forum_db->fetch_assoc($result);
+            $timestamp =  $res['posted'];
+        } else
+            $timestamp = 'fail';
 
 
-	if(FORUM_PAGE == 'index' && !$mob[0]) {
+	if(FORUM_PAGE == 'index') {
 		$forum_head['jquery'] = '<script type="text/javascript" src="'.$base_url.'/extensions/msgflow/js/jquery-1.4.2.min.js"></script>';
 		$forum_head['jbeep'] = '<script type="text/javascript" src="'.$base_url.'/extensions/msgflow/js/jquery-jbeep-0.1.js"></script>';
 		$forum_head['json2'] = '<script type="text/javascript" src="'.$base_url.'/extensions/msgflow/js/json2.js"></script>';
 		$forum_head['msgflow'] = '<script type="text/javascript" src="'.$base_url.'/extensions/msgflow/js/msgflow_index.js"></script>';
 		$forum_head['msgflow_timestamp'] = '<script type="text/javascript">'.
-			'var msgflow_timestamp = '.time().';'.
+			'var msgflow_timestamp = '.$timestamp.';'.
 			'var msgflow_document_title = "";'.
                         'var msgflow_extern = "'.$base_url.'/extern.php"'.
                         '</script>';
