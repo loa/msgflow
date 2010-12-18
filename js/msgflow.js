d9e176bac519b0d42aaa09248d8804802f92605e
Carl Loa Odin <carlodin@gmail.com>
2010-12-18 15:30:38 +0100

    can now handle all existing topics in viewforum, including topics

--- js/msgflow.js
+++ js/msgflow.js
@@ -6,13 +6,7 @@ $(document).ready(function() {
 
 function msgflow_pull() {
     // Get updates for index page
-    if(msgflow_forum_page == "index") {
-        $.getJSON(msgflow_base_url+'/extern.php?action=msgflow&from=' + msgflow_timestamp, msgflow_index);
-    }
-    // Get updates for viewforum
-    else if(msgflow_forum_page == 'viewforum') {
-        $.getJSON(msgflow_base_url+'/extern.php?action=msgflow&from=' + msgflow_timestamp, msgflow_viewforum);
-    }
+    $.getJSON(msgflow_base_url+'/extern.php?action=msgflow&from=' + msgflow_timestamp, msgflow_callback);
 }
 
 function msgflow_beep() {
@@ -27,55 +21,27 @@ function msgflow_beep() {
     }
 }
 
-function msgflow_index(data) {
+function msgflow_callback(data) {
     var last_timestamp = msgflow_timestamp;
 
-    // For each forum
+    // For each update
     $(data.updates).each(function () {
         // Check if this post is new
         if(this.posted > msgflow_timestamp) {
             // Increase the timestamp
             last_timestamp = this.posted;
-            
-            // Get the forum_row for faster dom lookups
-            var forum_row = $("#forum" + this.forum_id);
-            
-            // Make the icon change to new posts
-            $(forum_row)
-                .addClass("new")
-                .children("span.icon")
-                .addClass("new");
-            
-            // Update num_topics
-            $(forum_row).find("li.info-topics > strong")
-                .text(this.num_topics);
-            
-            // Update num_posts
-            $(forum_row).find("li.info-posts > strong")
-                .text(this.num_posts);
-            
-            // Update lastpost date and url
-            $(forum_row).find("li.info-lastpost > strong")
-                .html('<a href="' + this.post_url + '">' + this.date + '</a>');
-            
-            // Check if last poster exists (empty forum)
-            if($(forum_row).find("li.info-lastpost > cite").length == 0)
-                // Add last poster cite
-                $("<cite/>").insertAfter($(forum_row).find("li.info-lastpost > strong"));
-                    
-            // Update last poster name
-            $(forum_row).find("li.info-lastpost > cite")
-                .text('by ' + this.poster);
-            
-            // Add "( New posts)" link on the side of forum title
-            if($(forum_row).children("div.item-subject").find("small").length == 0) {
-                var newpost = $("<small/>");
-                $(newpost)
-                    .html(' ( <a title="This forum contain posts made since your last visit." ' +
-                          'href="' + this.forum_newpost_url + '">' +
-                          'New posts</a> )')
-                    .insertAfter($(forum_row).children("div.item-subject").find("a"));
-            }			
+
+            // Update index
+            if(msgflow_forum_page == 'index') {
+                msgflow_index(this);
+            }
+
+            // Update viewforum
+            else if(msgflow_forum_page == 'viewforum') {
+                if(msgflow_forum_id == this.forum_id)
+                    msgflow_viewforum(this);
+            }
+ 
         }
     });
     
@@ -99,27 +65,158 @@ function msgflow_index(data) {
             msgflow_pull();
 }
 
-function msgflow_viewforum() {
-    var last_timestamp = msgflow_timestamp;
-
-
-    // Make sure we got new updates
-    if(msgflow_timestamp < last_timestamp) {
-            // Set the new timestamp
-            msgflow_timestamp = last_timestamp;
+function msgflow_index(data) {
+    // Get the forum_row for faster dom lookups
+    var forum_row = $("#forum" + data.forum_id);
+    
+    // Make the icon change to new posts
+    $(forum_row)
+        .addClass("new")
+        .children("span.icon")
+        .addClass("new");
+    
+    // Update num_topics
+    $(forum_row).find("li.info-topics > strong")
+        .text(data.num_topics);
+    
+    // Update num_posts
+    $(forum_row).find("li.info-posts > strong")
+        .text(data.num_posts);
+    
+    // Update lastpost date and url
+    $(forum_row).find("li.info-lastpost > strong")
+        .html('<a href="' + data.post_url + '">' + data.date + '</a>');
+    
+    // Check if last poster exists (empty forum)
+    if($(forum_row).find("li.info-lastpost > cite").length == 0)
+        // Add last poster cite
+        $("<cite/>").insertAfter($(forum_row).find("li.info-lastpost > strong"));
             
-            // Sound notification
-            msgflow_beep();
- 
-            // Check if we saved the original document title
-            if(!msgflow_document_title)
-                    msgflow_document_title = document.title;
+    // Update last poster name
+    $(forum_row).find("li.info-lastpost > cite")
+        .text('by ' + data.poster);
+    
+    // Add "( New posts)" link on the side of forum title
+    if($(forum_row).children("div.item-subject").find("small").length == 0) {
+        var newpost = $("<small/>");
+        $(newpost)
+            .html(' ( <a title="This forum contain posts made since your last visit." ' +
+                'href="' + data.forum_newpost_url + '">' +
+                'New posts</a> )')
+            .insertAfter($(forum_row).children("div.item-subject").find("a"));
+    }			
+}
+
+function msgflow_viewforum(data) {
+    // Check if topic already is visible
+    if ($("#topic"+data.topic_id).length > 0) {
+        // Get the forum_row for faster dom lookups
+        var topic_row = $("#topic" + data.topic_id);
+       
+        // Make the icon change to new posts
+        $(topic_row)
+            .addClass("new")
+            .children("span.icon")
+            .addClass("new");
+
+        // Update num_replies
+        $(topic_row).find("li.info-replies > strong")
+            .text(data.num_replies);
+
+        // Update num_views
+        $(topic_row).find("li.info-views > strong")
+            .text(data.num_views);        
+
+        // Update lastpost date and url
+        $(topic_row).find("li.info-lastpost > strong")
+            .html('<a href="' + data.post_url + '">' + data.date + '</a>');
+    
+        // Update last poster name
+        $(topic_row).find("li.info-lastpost > cite")
+            .text('by ' + data.poster);
+
+        // Add "( New posts )" link on the side of the topic creators name
+        if ($(topic_row).children("div.item-subject").find("span.item-nav").length == 0) {
+            var newpost = $("<span/>");
+            $(newpost)
+                .addClass("item-nav")
+                .html(
+                    ' ( <em class="item-newposts">' +
+                    ' <a href="' + data.topic_newpost_url + '">New posts</a>' +
+                    '</em> )'
+                )
+                .insertAfter($(topic_row).find("span.item-starter"));
+        } 
+
+        // Check if topic is sticky
+        if ($(topic_row).hasClass("sticky")) {
+            // Remove class main-first-item from top_row
+            $("div.main-item:first").removeClass("main-first-item");
             
-            // Make notification in titlebar
-            document.title = 'New posts! - ' + msgflow_document_title;
+            // Add main-first-item and add move to top
+            $(topic_row)
+                .addClass("main-first-item")
+                .prependTo($("div.main-content"));
+        }
+        
+        // Topic is not sticky
+        else {
+            // Get the topic beneath stickies as rel_row
+            var rel_row = $("div.main-item").not(".sticky").filter(":first");
+            
+            // Make sure we found some other topic
+            if($(rel_row).length > 0 && $(rel_row)[0] != $(topic_row)[0]) {
+                // Remove main-first-item class from rel_row
+                $(rel_row).removeClass("main-first-item");
+     
+                // Insert topic_row before rel_row
+                $(topic_row).insertBefore($(rel_row));
+            } 
+
+            // Add class main-first-item to top_row
+            $("div.main-item:first").addClass("main-first-item");
+        } 
     }
-    // Make new long pull
-    if(data.status == "success")
-            msgflow_pull();
+   
+    // Topic is not visible, create new row
+    else {
+        // Create full row....
 
+        // Check if there are more than allowed topics on the page visible
+        if (true) {
+            // Remove the topic in the bottom of the page
+        }
+        
+        // Get the topic beneath stickies as rel_row
+        var rel_row = $("div.main-item").not(".sticky").filter(":first");
+        
+        // Make sure there existed none stickies
+        if($(rel_row).length == 0) {
+            // Only stickies, take the last one
+            rel_row = $("div.main-item:last");
+        }
+
+        // Remove main-first-item class from rel_row
+        // todo
+        
+        // Insert topic_row before rel_row
+        // todo
+        
+        // Add class main-first-item to top_row
+        // todo
+    }
+
+    // Remove classes even and odd from all div.main-item
+    $("div.main-item")
+        .removeClass("even")
+        .removeClass("odd");
+
+    // Set classes even and odd to corrent div.main-item
+    // Jquery even is 0 indexed so its reverse for us
+    $("div.main-item")
+        .filter(":even")
+            .addClass("odd")
+        .end()
+        .filter(":odd")
+            .addClass("even");
 }
